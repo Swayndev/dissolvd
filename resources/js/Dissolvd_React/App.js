416094fd9dec24d2940a436e8b0a757ee5ca2039
Jayke Alan <jaykegriffiths@gmail.com>
2022-03-24 10:32:09 +0100

    started work on create account page

--- resources/js/Dissolvd_React/App.js
+++ resources/js/Dissolvd_React/App.js
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from "react";
-// import '../../css/app.scss';
+import "../../css/app.scss";
 import Body from "./Components/Body";
-// import app from "../css/app.scss"
-
 import { BrowserRouter as Router } from "react-router-dom";
-// import { Fragment } from 'react/cjs/react.production.min';
 
 function App() {
     return (
---
+++ resources/js/Dissolvd_React/Components/CreateAccountPage/CreateAccountPage.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./CreateAccountPage.css";
+
+export const CreateAccountPage = () => {
+    return (
+        <div className="create-wrap">
+            <div className="create-header">
+                <h1>How does it work?</h1>
+            </div>
+            <div className="create-goal">
+                <h3>Our Goal</h3>
+                <p>
+                    lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                </p>
+                <p>
+                    lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                </p>
+                <p>
+                    lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                </p>
+            </div>
+            <div className="create-watch">
+                <div className="create-watch-heading">
+                    <p>step one</p>
+                    <h3>Watch</h3>
+                </div>
+                <img className="create-watch-img" src="" alt=""></img>
+                <p className="create-watch-text">
+                    lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                </p>
+            </div>
+        </div>
+    );
+};
